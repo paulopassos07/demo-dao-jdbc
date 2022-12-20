5022b97f0505a652fef09e6e931da019862e1f71
Paulo César Passos Pereira <paulo.passos.pereira1@outlook.com>
2022-12-20 08:45:14 -0300

    implement delete

--- src/application/Main.java
+++ src/application/Main.java
@@ -7,20 +7,24 @@ import model.entities.Seller;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
         SellerDao sellerDao = DaoFactory.createSellerDao();
+
         System.out.println("=== TEST 1: seller findById =====");
-        Seller seller1 = sellerDao.findById(3);
-        System.out.println(seller1);
+        Seller seller = sellerDao.findById(3);
+        System.out.println(seller);
 
         System.out.println("\n=== TEST 2: seller findByDepartment =====");
         Department department = new Department(2, null);
         List<Seller> list = sellerDao.findByDepartment(department);
-        for (Seller seller2 : list){
-            System.out.println(seller2);
+        for (Seller obj : list) {
+            System.out.println(obj);
         }
 
         System.out.println("\n=== TEST 3: seller findAll =====");
@@ -35,9 +39,17 @@ public class Main {
         System.out.println("Inserted! New id = " + newSeller.getId());
 
         System.out.println("\n=== TEST 5: seller update =====");
-        seller3 = sellerDao.findById(1);
-        seller3.setName("Martha Waine");
-        sellerDao.update(seller3);
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Waine");
+        sellerDao.update(seller);
         System.out.println("Update completed");
+
+        System.out.println("\n=== TEST 6: seller delete =====");
+        System.out.println("Enter id for delete test: ");
+        int id = sc.nextInt();
+        sellerDao.deleteById(id);
+        System.out.println("Delete completed");
+
+        sc.close();
     }
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -89,6 +89,20 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public void deleteById(Integer id) {
+        PreparedStatement st = null;
+
+        try {
+            st = conn.prepareStatement("DELETE FROM seller WHERE Id = ?");
+
+            st.setInt(1, id);
+            st.executeQuery();
+        }
+        catch (SQLException e){
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(st);
+        }
 
     }
 
